6f9c603946940c9e5c8298de1fe619b43df68943
Chan <smartwebstar18@gmail.com>
2022-09-26 16:57:23 -0400

    project serum

---
+++ lang/src/common.rs
@@ -0,0 +1,14 @@
+use crate::Result;
+use solana_program::account_info::AccountInfo;
+use solana_program::system_program;
+
+pub fn close<'info>(info: AccountInfo<'info>, sol_destination: AccountInfo<'info>) -> Result<()> {
+    // Transfer tokens from the account to the sol_destination.
+    let dest_starting_lamports = sol_destination.lamports();
+    **sol_destination.lamports.borrow_mut() =
+        dest_starting_lamports.checked_add(info.lamports()).unwrap();
+    **info.lamports.borrow_mut() = 0;
+
+    info.assign(&system_program::ID);
+    info.realloc(0, false).map_err(Into::into)
+}
